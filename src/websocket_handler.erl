4e841be5596133fd56bf994ef758a54c44ebbd90
Patricio León M <patricioleonm@gmail.com>
2020-03-31 23:53:32 -0300

    lager added and messaged changed

--- src/websocket_handler.erl
+++ src/websocket_handler.erl
@@ -4,21 +4,28 @@
 -export([websocket_init/1]).
 -export([websocket_handle/2]).
 -export([websocket_info/2]).
+-compile([{parse_transform, lager_transform}]).
 
 init(Req, Opts) ->
+	Headers = maps:get(headers, Req),
+	Key = maps:get(<<"sec-websocket-key">>, Headers),
+	lager:info("init with key :~s", [Key]),
 	{cowboy_websocket, Req, Opts}.
 
 websocket_init(State) ->
+	lager:info("websocket init [~s]", [State] ),
 	erlang:start_timer(1000, self(), <<"Hello!">>),
 	{[], State}.
 
 websocket_handle({text, Msg}, State) ->
-	{[{text, << "That's what she said! ", Msg/binary >>}], State};
+	lager:info("websocket handler [~s]", [State]),
+	{[{text, << "Message from websocket_handler :", Msg/binary >>}], State};
 websocket_handle(_Data, State) ->
 	{[], State}.
 
 websocket_info({timeout, _Ref, Msg}, State) ->
-	erlang:start_timer(1000, self(), <<"How' you doin'?">>),
+	lager:info("socket info [~s][~s]", [Msg, State]),
+	erlang:start_timer(1000, self(), <<"this is info every 1 sec">>),
 	{[{text, Msg}], State};
 websocket_info(_Info, State) ->
 	{[], State}.
