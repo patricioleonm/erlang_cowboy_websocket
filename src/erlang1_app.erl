842de2865bc95a11244dcb1eee79c2b97951f96d
Patricio León M <patricioleonm@gmail.com>
2020-03-31 23:52:15 -0300

    route added for static content

--- src/erlang1_app.erl
+++ src/erlang1_app.erl
@@ -14,8 +14,10 @@ start(_StartType, _StartArgs) ->
     lager:set_loglevel(lager_console_backend, debug),
     Dispatch = cowboy_router:compile([
         {'_', [
-            {"/", cowboy_static, {priv_file, erlang1, "index.html"} }]},
-            {"/websocket", websocket_handler, []}
+            {"/", cowboy_static, {priv_file, erlang1, "index.html"} },            
+            {"/websocket", websocket_handler, []},
+            {"/[...]", cowboy_static, {priv_dir, erlang1, ""} }
+        ]}
     ]),
     {ok, _} = cowboy:start_clear(my_http_listener,
         [{port, 8080}],
